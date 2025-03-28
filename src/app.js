63e62650e7ac415698250001b6cd2c9c5bc2416d
Julia Baur <juliabaur1@gmx.de>
2025-03-28 15:51:36 +0100

    Update app.js to Use Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const routes = require('./routes');
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// API Routes
+app.use('/api', routes);
 
 // Routes
 app.get('/', (req, res) => {
