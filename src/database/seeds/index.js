fe0fc51d97e8eb47cbb463139bb0e7627a2a82f3
Julia Baur <juliabaur1@gmx.de>
2025-03-28 17:02:33 +0100

    added scripts to create more dummy users and chat messages

--- src/database/seeds/index.js
+++ src/database/seeds/index.js
@@ -2,6 +2,8 @@ const seedUsers = require('./01_users');
 const seedTags = require('./02_tags');
 const seedTeams = require('./03_teams');
 const seedBadges = require('./04_badges');
+const seedMoreUsers = require('./05_more_users');
+const seedMessages = require('./06_messages');
 
 const runSeeds = async () => {
   try {
@@ -11,6 +13,8 @@ const runSeeds = async () => {
     await seedTags();
     await seedTeams();
     await seedBadges();
+    await seedMoreUsers();
+    await seedMessages();
     
     console.log('Database seeding completed successfully!');
   } catch (error) {
