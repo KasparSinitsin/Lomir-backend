76898d8791d829bab9c470e816e1f3d0cb69e847
Julia Baur <juliabaur1@gmx.de>
2025-04-04 16:40:53 +0200

    fixed autenticateToken in badgeRoutes and messageRoutes.js

--- src/routes/badgeRoutes.js
+++ src/routes/badgeRoutes.js
@@ -1,10 +1,11 @@
+const express = require('express');
 const badgeController = require('../controllers/badgeController');
 const { authenticateToken } = require('../middlewares/auth');
 
 const router = express.Router();
- 
+
 router.get('/', badgeController.getAllBadges);
-router.post('/award', authenticateToken, badgeController.awardBadge); // Protected
+router.post('/award', authenticateToken, badgeController.awardBadge);
 router.get('/user/:userId', badgeController.getUserBadges);
 
 module.exports = router;
