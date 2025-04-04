8b5c1f4b9926a4f9007abba52b0387f65bee4782
Julia Baur <juliabaur1@gmx.de>
2025-04-04 16:38:44 +0200

    fixed teamRoutes.js with auth.authenticateToken

--- src/routes/badgeRoutes.js
+++ src/routes/badgeRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
 const badgeController = require('../controllers/badgeController');
+const { authenticateToken } = require('../middlewares/auth');
 
 const router = express.Router();
-
+ 
 router.get('/', badgeController.getAllBadges);
-router.post('/award', badgeController.awardBadge);
+router.post('/award', authenticateToken, badgeController.awardBadge); // Protected
 router.get('/user/:userId', badgeController.getUserBadges);
 
 module.exports = router;
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const teamController = require('../controllers/teamController');
-const auth = require('../middlewares/auth'); // assuming you want team actions protected
+const auth = require('../middlewares/auth');
 
 // Team routes
-router.post('/', auth, teamController.createTeam); // protect team creation
+router.post('/', auth.authenticateToken, teamController.createTeam);
 router.get('/', teamController.getAllTeams);
 router.get('/:id', teamController.getTeamById);
-router.put('/:id', auth, teamController.updateTeam); // protect updates
-router.delete('/:id', auth, teamController.deleteTeam); // protect deletions
-router.post('/:id/members', auth, teamController.addTeamMember);
-router.delete('/:id/members/:userId', auth, teamController.removeTeamMember);
+router.put('/:id', auth.authenticateToken, teamController.updateTeam); // Fixed
+router.delete('/:id', auth.authenticateToken, teamController.deleteTeam); // Fixed
+router.post('/:id/members', auth.authenticateToken, teamController.addTeamMember); // Fixed
+router.delete('/:id/members/:userId', auth.authenticateToken, teamController.removeTeamMember); // Fixed
 
 module.exports = router;
