57c7d26a65c3763477e22ef70019b13c2cb1e53b
Julia Baur <juliabaur1@gmx.de>
2025-04-11 00:46:31 +0200

    corrected searchRoutes.js

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -4,10 +4,9 @@ const { authenticateToken } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', searchController.globalSearch); //Public
-
-router.get('/', authenticateToken, searchController.search); //Protected
-router.get('/by-tag/:tagId', authenticateToken, searchController.searchByTag); //Protected
-router.get('/by-location', authenticateToken, searchController.searchByLocation); //Protected
+router.get('/global', searchController.globalSearch); // Public global search
+router.get('/', authenticateToken, searchController.search); // Protected general search
+router.get('/by-tag/:tagId', authenticateToken, searchController.searchByTag); // Protected tag search
+router.get('/by-location', authenticateToken, searchController.searchByLocation); // Protected location search
 
 module.exports = router;
