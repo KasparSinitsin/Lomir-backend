6153ff387308de7bac74ac7f368229a90b91b945
Julia Baur <juliabaur1@gmx.de>
2026-05-17 18:14:16 +0200

    remove unused legacy search routes

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const searchController = require('../controllers/searchController');
-const { authenticateToken, optionalAuthenticateToken } = require('../middlewares/auth');
+const { optionalAuthenticateToken } = require('../middlewares/auth');
 
 const router = express.Router();
 
 // Use optional authentication for routes that can work with or without authentication
 router.get('/all', optionalAuthenticateToken, searchController.getAllUsersAndTeams);
 router.get('/global', optionalAuthenticateToken, searchController.globalSearch);
-
-// These routes require authentication
-router.get('/', authenticateToken, searchController.search);
-router.get('/by-tag/:tagId', authenticateToken, searchController.searchByTag);
-router.get('/by-location', authenticateToken, searchController.searchByLocation);
 
 module.exports = router;
