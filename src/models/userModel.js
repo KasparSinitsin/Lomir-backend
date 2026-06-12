e233dc56a83444eea59e04324f91ec79412aee73
Julia Baur <juliabaur1@gmx.de>
2026-06-12 18:06:44 +0200

    feat: make new profiles hidden by default until email verified

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -93,7 +93,7 @@ const userModel = {
           created_at,
           updated_at
         )
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,FALSE,TRUE,NOW(),NOW())
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,FALSE,FALSE,NOW(),NOW())
         RETURNING ${CREATED_USER_FIELDS}
         `,
         [
